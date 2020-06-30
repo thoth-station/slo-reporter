42f137734fc8d7fbc839c7d02ad96862f1f12d2f
Francesco Murdaca <fmurdaca@redhat.com>
2020-06-30 10:42:05 +0200

    Add method and standardize Kebechet SLI
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- thoth/slo_reporter/sli_kebechet.py
+++ thoth/slo_reporter/sli_kebechet.py
@@ -73,14 +73,14 @@ class SLIKebechet(SLIBase):
         """
         html_inputs = {}
 
-        for knowledge_quantity in _REGISTERED_KEBECHET_QUANTITY.keys():
-            html_inputs[knowledge_quantity] = {}
-            html_inputs[knowledge_quantity]['name'] = _REGISTERED_KEBECHET_QUANTITY[knowledge_quantity]
+        for knowledge_quantity in sli.keys():
 
             if sli[knowledge_quantity] != "ErrorMetricRetrieval":
-                html_inputs[knowledge_quantity]['value'] = int(sli[knowledge_quantity])
+                html_inputs[knowledge_quantity] = int(sli[knowledge_quantity])
             else:
-                html_inputs[knowledge_quantity]['value'] = np.nan
+                html_inputs[knowledge_quantity] = np.nan
+
+            html_inputs.append([knowledge_quantity, value])
 
         return html_inputs
 
@@ -89,7 +89,7 @@ class SLIKebechet(SLIBase):
 
         @param sli: It's a dict of SLI associated with the SLI type.
         """
-        html_inputs = self._evaluate_sli(sli=sli)
+        html_inputs = _evaluate_sli(sli=sli)
         report = HTMLTemplates.thoth_kebechet_template(html_inputs=html_inputs)
 
         return report
